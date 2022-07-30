4a44ea28fb94a4cc9da349308feafbf2abfd9087
Seweryn Michota <sewerynmichota@hod1176812.local>
2022-07-30 22:06:53 +0100

    Add ButtonIcon

---
+++ src/components/atoms/ButtonIcon/ButtonIcon.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ButtonIcon = styled.button`
+  height: 67px;
+  width: 67px;
+  border-radius: 20px;
+  background-image: url(${({ icon }) => icon});
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 40%;
+  border: none;
+  cursor: pointer;
+  background-color: ${({ active }) => (active ? 'white' : 'transparent')};
+`;
+
+export default ButtonIcon;
---
+++ src/components/atoms/ButtonIcon/ButtonIcon.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ButtonIcon from './ButtonIcon';
+import styled from 'styled-components';
+
+import bulbIcon from 'assets/icons/bulb.svg';
+import linkIcon from 'assets/icons/link.svg';
+import logoutIcon from 'assets/icons/logout.svg';
+import magnifierIcon from 'assets/icons/magnifier.svg';
+import penIcon from 'assets/icons/pen.svg';
+import plusIcon from 'assets/icons/plus.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
+
+const YellowBackgound = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 500px;
+  background: ${({ theme }) => theme.primary};
+`;
+
+storiesOf('ButtonIcon', module)
+  .addDecorator((story) => <YellowBackgound>{story()}</YellowBackgound>)
+  .add('Bulb', () => <ButtonIcon icon={bulbIcon} />)
+  .add('Bulb Active ', () => <ButtonIcon active icon={bulbIcon} />)
+  .add('Logout', () => <ButtonIcon icon={logoutIcon} />)
+  .add('Pen', () => <ButtonIcon icon={penIcon} />)
+  .add('Plus', () => <ButtonIcon icon={plusIcon} />)
+  .add('Twitter', () => <ButtonIcon icon={twitterIcon} />);
